1dfafaa3e3def533c7aefe565e77be21565a5dc8
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-12-17 16:03:36 +0500

    Initial commit

---
+++ app/src/main/java/uz/unzosoft/composecleanarchitecture/MainActivity.kt
@@ -0,0 +1,22 @@
+package uz.unzosoft.composecleanarchitecture
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import uz.unzosoft.composecleanarchitecture.presentation.ui.theme.ComposeCleanArchitectureTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCleanArchitectureTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
